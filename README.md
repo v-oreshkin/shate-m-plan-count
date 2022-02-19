7d1488e118157e238efc90633492dea3a4578771
Vadim Oreshkin <v.oreshkin94@gmail.com>
2022-02-19 19:03:07 +0300

    Create flutter project

---
+++ README.md
@@ -0,0 +1,4 @@
+# shate_m_plan_count
+
+Application for count daily Shate-M plan
+
---
+++ android/app/src/main/kotlin/com/example/oreskin/by/shate_m_plan_count/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oreskin.by.shate_m_plan_count
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
